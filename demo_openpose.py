89e0ac79584bf0b3a76b24aa3d6d4af5a8a24ead
Qiancheng Zhao <felixchenfy@gmail.com>
2019-03-30 17:53:23 -0500

    cleaned code of openpose and siammask

--- demo_openpose.py
+++ demo_openpose.py
@@ -6,37 +6,28 @@ import simplejson
 import argparse
 import math
 
-import mylib.io as myio
+# Mine ==============================================================
+
 from mylib.displays import drawActionResult
+from mylib.io import DataLoader_usbcam, DataLoader_folder
 import mylib.funcs as myfunc
-import mylib.feature_proc as myproc 
-from mylib.action_classifier import *
+# import mylib.feature_proc as myproc 
+# from mylib.action_classifier import *
 
-# PATHS ==============================================================
-
+# Openpose ==============================================================
+from mylib.openpose import SkeletonDetector
 CURR_PATH = os.path.dirname(os.path.abspath(__file__))+"/"
 
+# Set input =================================
 
-# INPUTS ==============================================================
-
-def parse_input_method():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--source", required=False, default='webcam',
-        help="Choose from (1) webcam, (2) folder")
-    return parser.parse_args().source
- 
-arg_input = parse_input_method()
-FROM_WEBCAM = arg_input == "webcam" # from web camera
-FROM_FOLDER = arg_input == "folder" # read images from a folder
-
-# PATHS and SETTINGS =================================
+FROM_WEBCAM = True
 
 if FROM_WEBCAM:
     save_idx = "4"
     DO_INFER_ACTIONS =  False
     SAVE_RESULTANT_SKELETON_TO_TXT_AND_IMAGE = True
-    image_size = "304x240" # 14 fps
-    # image_size = "240x208" # 14 fps
+    openpose_image_size = "304x240" # 14 fps
+    # openpose_image_size = "240x208" # 14 fps
     OpenPose_MODEL = ["mobilenet_thin", "cmu"][1]
 
 elif FROM_FOLDER:
@@ -49,239 +40,24 @@ elif FROM_FOLDER:
         return "/home/qiancheng/DISK/feiyu/TrainYolo/data_yolo/video_bottle/images/", 2
     SRC_IMAGE_FOLDER, SKIP_NUM_IMAGES = set_source_images_from_folder()
     save_idx += SRC_IMAGE_FOLDER.split('/')[-2] # plus file name
-    image_size = "304x240" # 14 fps
+    openpose_image_size = "304x240" # 14 fps
     OpenPose_MODEL = ["mobilenet_thin", "cmu"][1]
 
 else:
     assert False
 
-DRAW_FPS = True
-
-# Openpose ==============================================================
-
-sys.path.append(CURR_PATH + "./tf-pose-estimation")
-from tf_pose.networks import get_graph_path, model_wh
-from tf_pose.estimator import TfPoseEstimator
-from tf_pose import common
-
-logger = logging.getLogger('TfPoseEstimator')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-
-# Human pose detection ==============================================================
-
-class SkeletonDetector(object):
-    # This func is mostly copied from https://github.com/ildoonet/tf-pose-estimation
-
-    def __init__(self, model=None, image_size=None):
-        
-        if model is None:
-            model = "cmu"
-
-        if image_size is None:
-            image_size = "432x368" # 7 fps
-            # image_size = "336x288"
-            # image_size = "304x240" # 14 fps
-
-        models = set({"mobilenet_thin", "cmu"})
-        self.model = model if model in models else "mobilenet_thin"
-        # parser = argparse.ArgumentParser(description='tf-pose-estimation run')
-        # parser.add_argument('--image', type=str, default='./images/p1.jpg')
-        # parser.add_argument('--model', type=str, default='cmu', help='cmu / mobilenet_thin')
-
-        # parser.add_argument('--resize', type=str, default='0x0',
-        #                     help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
-        # parser.add_argument('--resize-out-ratio', type=float, default=4.0,
-        #                     help='if provided, resize heatmaps before they are post-processed. default=1.0')
-        self.resize_out_ratio = 4.0
-
-        # args = parser.parse_args()
-
-        w, h = model_wh(image_size)
-        if w == 0 or h == 0:
-            e = TfPoseEstimator(get_graph_path(self.model),
-                                target_size=(432, 368))
-        else:
-            e = TfPoseEstimator(get_graph_path(self.model), target_size=(w, h))
-
-        # self.args = args
-        self.w, self.h = w, h
-        self.e = e
-        self.fps_time = time.time()
-        self.cnt_image = 0
-
-    def detect(self, image):
-        self.cnt_image += 1
-        if self.cnt_image == 1:
-            self.image_h = image.shape[0]
-            self.image_w = image.shape[1]
-            self.scale_y = 1.0 * self.image_h / self.image_w
-        t = time.time()
-
-        # Inference
-        humans = self.e.inference(image, resize_to_default=(self.w > 0 and self.h > 0),
-                                #   upsample_size=self.args.resize_out_ratio)
-                                  upsample_size=self.resize_out_ratio)
-
-        # Print result and time cost
-        elapsed = time.time() - t
-        logger.info('inference image in %.4f seconds.' % (elapsed))
-
-        return humans
-    
-    def draw(self, img_disp, humans):
-        img_disp = TfPoseEstimator.draw_humans(img_disp, humans, imgcopy=False)
-
-        # logger.debug('show+')
-        if DRAW_FPS:
-            cv2.putText(img_disp,
-                        # "Processing speed: {:.1f} fps".format( (1.0 / (time.time() - self.fps_time) )),
-                        "fps = {:.1f}".format( (1.0 / (time.time() - self.fps_time) )),
-                        (10, 30),  cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        (0, 0, 255), 2)
-        self.fps_time = time.time()
-
-    def humans_to_skelsList(self, humans, scale_y = None): # get (x, y * scale_y)
-        if scale_y is None:
-            scale_y = self.scale_y
-        skelsList = []
-        NaN = 0
-        for human in humans:
-            skeleton = [NaN]*(18*2)
-            for i, body_part in human.body_parts.items(): # iterate dict
-                idx = body_part.part_idx
-                skeleton[2*idx]=body_part.x
-                skeleton[2*idx+1]=body_part.y * scale_y
-            skelsList.append(skeleton)
-        return skelsList
-    
-    @staticmethod
-    def get_ith_skeleton(skelsList, ith_skeleton=0):
-        res = np.array(skelsList[ith_skeleton])
-        return res
-
-
 # ==============================================================
 
-LENGTH_OF_IMAGE_INFO = 5 # see mylib/myio.py: [cnt_action, cnt_clip, cnt_image, action_type, filepath]
 
-class DataLoader_usbcam(object):
-    def __init__(self, max_framerate = 10):
-        self.cam = cv2.VideoCapture(0)
-        self.num_images = 9999999
-        self.frame_period = 1.0/max_framerate*0.999
-        self.prev_image_time = time.time() - self.frame_period
-
-    def wait_for_framerate(self):
-        t_curr = time.time()
-        t_wait = self.frame_period - (t_curr - self.prev_image_time)
-        if t_wait > 0:
-            time.sleep(t_wait)
-
-    def load_next_image(self):
-        self.wait_for_framerate()
-        
-        ret_val, img = self.cam.read()
-        self.prev_image_time = time.time()
-
-        img =cv2.flip(img, 1)
-        action_type = "unknown"
-        return img, action_type, ["none"]*LENGTH_OF_IMAGE_INFO
-
-class DataLoader_folder(object):
-    def __init__(self, folder, num_skip = 0):
-        self.cnt_image = 0
-        self.folder = folder
-        self.filenames = myfunc.get_filenames(folder, sort = True)
-        self.idx_step = num_skip + 1
-        self.num_images = int( len(self.filenames) / self.idx_step)
-
-    def load_next_image(self):
-        img =  cv2.imread(self.folder + self.filenames[self.cnt_image])
-        self.cnt_image += self.idx_step
-        action_type = "unknown"
-        return img, action_type, ["none"]*LENGTH_OF_IMAGE_INFO
-
-class DataLoader_txtscript(object):
-    def __init__(self, SRC_IMAGE_FOLDER, VALID_IMAGES_TXT):
-        self.images_info = myio.collect_images_info_from_source_images(SRC_IMAGE_FOLDER, VALID_IMAGES_TXT)
-        self.imgs_path = SRC_IMAGE_FOLDER
-        self.i = 0
-        self.num_images = len(self.images_info)
-        print("Reading images from txtscript: {}\n".format(SRC_IMAGE_FOLDER))
-        print("Reading images information from: {}\n".format(VALID_IMAGES_TXT))
-        print("    Num images = {}\n".format(self.num_images))
-
-    def save_images_info(self, path):
-        with open(path, 'w') as f:
-            simplejson.dump(self.images_info, f)
-
-    def load_next_image(self):
-        self.i += 1
-        filename = self.get_filename(self.i)
-        img = self.imread(self.i)
-        action_type = self.get_action_type(self.i)
-        return img, action_type, self.get_image_info(self.i)
-
-    def imread(self, index):
-        return cv2.imread(self.imgs_path + self.get_filename(index))
-    
-    def get_filename(self, index):
-        # [1, 7, 54, "jump", "jump_03-02-12-34-01-795/00240.png"]
-        # See "myio.collect_images_info_from_source_images" for the data format
-        return self.images_info[index-1][4] 
-
-    def get_action_type(self, index):
-        # [1, 7, 54, "jump", "jump_03-02-12-34-01-795/00240.png"]
-        # See "myio.collect_images_info_from_source_images" for the data format
-        return self.images_info[index-1][3]
-    
-    def get_image_info(self, index):
-        return self.images_info[index-1] # with a length of LENGTH_OF_IMAGE_INFO
-
-class OneObjTracker(object):
-    def __init__(self):
-        self.reset()
-
-    def track(self, skelsList):
-        N = len(skelsList)
-        if self.prev_skel is None:
-            res_idx = 0 # default is zero
-        else:
-            
-            dists = [0]*N
-            for i, skel in enumerate(skelsList):
-                dists[i] = self.measure_dist(self.prev_skel, skel)
-            min_dist = min(dists)
-            min_idx = dists.index(min_dist)
-            res_idx = min_idx
-        self.prev_skel = skelsList[res_idx].copy()
-        return res_idx
-
-    def measure_dist(self, prev_skel, curr_skel):
-        neck = 1*2
-        dx2 = (prev_skel[neck] - curr_skel[neck])**2
-        dy2 = (prev_skel[neck+1] - curr_skel[neck+1])**2
-        return math.sqrt(dx2+dy2)
-
-    def reset(self):
-        self.prev_skel = None
 
 if __name__ == "__main__":
  
     # -- Detect sekelton
-    my_detector = SkeletonDetector(OpenPose_MODEL, image_size)
+    my_detector = SkeletonDetector(OpenPose_MODEL, openpose_image_size)
 
     # -- Load images
     if FROM_WEBCAM:
         images_loader = DataLoader_usbcam()
-
     elif FROM_FOLDER:
         images_loader = DataLoader_folder(SRC_IMAGE_FOLDER, SKIP_NUM_IMAGES)
 
--- demo_siammask.py
+++ demo_siammask.py
@@ -4,40 +4,73 @@
 # Written by Qiang Wang (wangqiang2015 at ia.ac.cn)
 # --------------------------------------------------------
 import glob
-import sys, os
+import sys, os, cv2
+import numpy as np
 CURR_PATH = os.path.dirname(os.path.abspath(__file__))+"/"
 # sys.path.append(CURR_PATH)  # To find local version of the library\n",
-from SiamMask.tools.test import *
+import torch, argparse
+import SiamMask.tools.test as libsiam
 
-parser = argparse.ArgumentParser(description='PyTorch Tracking Demo')
 
-model_folder = 'SiamMask/experiments/siammask/'
-parser.add_argument('--resume', type=str, required=False,
-                    default = model_folder + 'SiamMask_DAVIS.pth',
-                    metavar='PATH',help='path to latest checkpoint (default: none)')
-parser.add_argument('--config', dest='config', default= model_folder + 'config_davis.json',
-                    help='hyper-parameter of SiamMask in json format')
-parser.add_argument('--base_path', default='SiamMask/data/my_bottle', help='datasets')
-args = parser.parse_args()
+def set_SiamMask_command_line_arguments():
+    parser = argparse.ArgumentParser(description='PyTorch Tracking Demo')
+    model_folder = 'SiamMask/experiments/siammask/'
+    parser.add_argument('--resume', type=str, required=False,
+                        default = model_folder + 'SiamMask_DAVIS.pth',
+                        metavar='PATH',help='path to latest checkpoint (default: none)')
+    parser.add_argument('--config', dest='config', default= model_folder + 'config_davis.json',
+                        help='hyper-parameter of SiamMask in json format')
+    parser.add_argument('--base_path', default='SiamMask/data/my_bottle', help='datasets')
+    args = parser.parse_args()
+    return args 
+
+class init_SiamMask_model(object):
+
+    def __init__(self):
+        # Setup device
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        torch.backends.cudnn.benchmark = True
+
+        # Setup Model
+        args = set_SiamMask_command_line_arguments()
+        cfg = libsiam.load_config(args)
+        from custom import Custom
+        siammask = Custom(anchors=cfg['anchors'])
+        if args.resume:
+            assert libsiam.isfile(args.resume), '{} is not a valid file'.format(args.resume)
+            siammask = libsiam.load_pretrain(siammask, args.resume)
+
+        siammask.eval().to(device)
+        
+        # -- Output
+        self.siammask = siammask
+        self.args = args
+        self.cfg = cfg
+        self.state = None
+
+    def init_tracker(self, im, x, y, w, h):
+        target_pos = np.array([x + w / 2, y + h / 2])
+        target_sz = np.array([w, h])
+        self.state = libsiam.siamese_init(im, target_pos, target_sz, self.siammask, self.cfg['hp'])  # init tracker
+    
+    def track(self, im):
+        self.state = libsiam.siamese_track(self.state, im, mask_enable=True, refine_enable=True)  # track
+        location = self.state['ploygon'].flatten()
+        ''' my notes:
+        print("\nstate:", state)
+        print("\nlocation:", location) # [364.17523 417.03296 240.6001  348.26935 365.40112 123.98926 488.97626 192.75287]
+        '''
+        mask = self.state['mask'] > self.state['p'].seg_thr
+        return mask, location
 
 if __name__ == '__main__':
-    # Setup device
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    torch.backends.cudnn.benchmark = True
-
-    # Setup Model
-    cfg = load_config(args)
-    from custom import Custom
-    siammask = Custom(anchors=cfg['anchors'])
-    if args.resume:
-        assert isfile(args.resume), '{} is not a valid file'.format(args.resume)
-        siammask = load_pretrain(siammask, args.resume)
-
-    siammask.eval().to(device)
+   
+    model_siammask = init_SiamMask_model()
 
     # Parse Image file
-    # img_files = sorted(glob.glob(join(args.base_path, '*.jp*')))
-    img_files = sorted(glob.glob(join(args.base_path, '*.png')))
+    # img_files = sorted(glob.glob(os.path.join(args.base_path, '*.jp*')))
+    image_folder = CURR_PATH + "images_bottle/"
+    img_files = sorted(glob.glob( image_folder + '*.png'))
     ims = [cv2.imread(imf) for imf in img_files]
 
     # Select ROI
@@ -52,27 +85,23 @@ if __name__ == '__main__':
     toc = 0
     for f, im in enumerate(ims):
         tic = cv2.getTickCount()
-        if f == 0:  # init
-            target_pos = np.array([x + w / 2, y + h / 2])
-            target_sz = np.array([w, h])
-            state = siamese_init(im, target_pos, target_sz, siammask, cfg['hp'])  # init tracker
-        elif f > 0:  # tracking
-            state = siamese_track(state, im, mask_enable=True, refine_enable=True)  # track
-            location = state['ploygon'].flatten()
-            ''' my notes:
-            print("\nstate:", state)
-            print("\nlocation:", location) # [364.17523 417.03296 240.6001  348.26935 365.40112 123.98926 488.97626 192.75287]
-            '''
-            mask = state['mask'] > state['p'].seg_thr
 
-            im[:, :, 2] = (mask > 0) * 255 + (mask == 0) * im[:, :, 2]
-            cv2.polylines(im, [np.int0(location).reshape((-1, 1, 2))], True, (0, 255, 0), 3)
-            cv2.imshow('SiamMask', im)
-            key = cv2.waitKey(1)
-            if key > 0:
-                break
+        if f == 0:  # init
+            model_siammask.init_tracker(im, x, y, w, h)
+
+        elif f > 0:  # tracking
+            mask, location = model_siammask.track(im)
+          
+            if 1: # Draw resultz
+                im[:, :, 2] = (mask > 0) * 255 + (mask == 0) * im[:, :, 2]
+                cv2.polylines(im, [np.int0(location).reshape((-1, 1, 2))], True, (0, 255, 0), 3)
+                cv2.imshow('SiamMask', im)
+                key = cv2.waitKey(1)
+                if key > 0:
+                    break
 
         toc += cv2.getTickCount() - tic
+
     toc /= cv2.getTickFrequency()
     fps = f / toc
     print('SiamMask Time: {:02.1f}s Speed: {:3.1f}fps (with visulization!)'.format(toc, fps))
